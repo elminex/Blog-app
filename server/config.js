cf733f34145eec5740d48e8e69f02d097b423a03
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-08-04 11:57:15 +0200

    Add config vars

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PORT: 8000,
-  DB: 'mongodb+srv://databaseUser:devilek123@cluster0-5rk77.mongodb.net/test?retryWrites=true&w=majority'
+  PORT: process.env.PORT,
+  DB: process.env.DB,
 }
